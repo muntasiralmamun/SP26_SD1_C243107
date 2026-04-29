d90bd6409c632e0149bc318965aca8b38cdd17c4
muntasiralmamun <muntasiralmamun6666@gmail.com>
2026-04-29 23:44:04 +0600

    Add files via upload

---
+++ R550.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class R550 {
+    public static void main(String [] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int x = (10*n);
+        System.out.print("The number of notebook : ");
+        System.out.print(x);
+    }
+}
